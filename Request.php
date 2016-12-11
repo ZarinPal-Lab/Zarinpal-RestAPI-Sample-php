0e4c0de2c9139da8fccb00be240bc3d4b4077037
Masoud Amini <amini141@gmail.com>
2016-12-11 13:10:34 +0330

    fix

--- Request.php
+++ Request.php
@@ -1,24 +1,29 @@
 <?php
-	$data = array('MerchantID' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 'Amount' => 10, 'CallbackURL' => 'http://www.google.com/', 'Description' => 'تست');
-	$jsonData = json_encode($data);
-	$ch = curl_init('https://www.zarinpal.com/pg/rest/WebGate/PaymentRequest.json');
-	curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v1');
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		'Content-Type: application/json',
-		'Content-Length: ' . strlen($jsonData)
-	));
 
-	$result = curl_exec($ch);
-	$err = curl_error($curl);
+$data = array('MerchantID' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 'Amount' => 100, 'CallbackURL' => 'http://www.YourSite.com/', 'Description' => 'خرید تست');
+$jsonData = json_encode($data);
+$ch = curl_init('https://www.zarinpal.com/pg/rest/WebGate/PaymentRequest.json');
+curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v1');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($jsonData)
+));
 
-	curl_close($curl);
+$result = curl_exec($ch);
+$err = curl_error($curl);
+$result = json_decode($result, true);
+curl_close($curl);
 
-	if ($err) {
-	  echo "cURL Error #:" . $err;
-	} else {
-	  echo $result;
-	}
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    if ($result["Status"] == 100) {
+        header('Location: https://www.zarinpal.com/pg/StartPay/' . $result["Authority"]);
+    } else {
+        echo'ERR: ' . $result["Status"];
+    }
+}
 ?>
