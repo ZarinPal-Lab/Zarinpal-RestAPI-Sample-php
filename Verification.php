1d14a4e267f1183eafbe2a0429a2fb45eb6ca8ed
Masoud Amini <amini141@gmail.com>
2016-12-11 13:26:15 +0330

    fix

--- Verification.php
+++ Verification.php
@@ -1,22 +1,29 @@
 <?php
-	$data = array('MerchantID' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 'Authority' => '000000000000000000000000000000123465', 'Amount' => 100);
-	$jsonData = json_encode($data);
-	$ch = curl_init('https://www.zarinpal.com/pg/restful/WebGate/PaymentVerification');
-	curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v1');
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		'Content-Type: application/json',
-		'Content-Length: ' . strlen($jsonData)
-	));
 
-	$result = curl_exec($ch);
-	curl_close($ch);
-	
-	if ($err) {
-	  echo "cURL Error #:" . $err;
-	} else {
-	  echo $result;
-	}
+$Authority = $_GET['Authority'];
+$data = array('MerchantID' => 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 'Authority' => $Authority, 'Amount' => 100);
+$jsonData = json_encode($data);
+$ch = curl_init('https://www.zarinpal.com/pg/rest/WebGate/PaymentVerification.json');
+curl_setopt($ch, CURLOPT_USERAGENT, 'ZarinPal Rest Api v1');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($jsonData)
+));
+
+$result = curl_exec($ch);
+curl_close($ch);
+$result = json_decode($result, true);
+
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    if ($result['Status'] == 100) {
+        echo 'Transation success. RefID:' . $result['RefID'];
+    } else {
+        echo 'Transation failed. Status:' . $result['Status'];
+    }
+}
 ?>
